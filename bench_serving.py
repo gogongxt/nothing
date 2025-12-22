dc94f4d6d2f7fc2455ce46886100602c28cac0db
XiaoTian <47113549+gogongxt@users.noreply.github.com>
2025-12-22 18:03:32 +0800

    Add ignore_eos option to request parameters

--- bench_serving.py
+++ bench_serving.py
@@ -325,6 +325,7 @@ async def async_request_openai_chat_completions(
             "temperature": 0.0,
             "max_tokens": request_func_input.output_len,
             "stream": not args.disable_stream,
+            "ignore_eos": not args.disable_ignore_eos,
             **request_func_input.extra_request_body,
         }
         headers = get_auth_headers()
