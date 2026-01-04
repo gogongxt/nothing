4f3faf1e53427d9c356202de5ba3dd3159af31fa
XiaoTian <47113549+gogongxt@users.noreply.github.com>
2026-01-04 14:23:22 +0800

    Add script to download ShareGPT dataset

---
+++ download_sharegpt.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -x
+
+curl -L -o ShareGPT_V3_unfiltered_cleaned_split.json https://huggingface.co/datasets/anon8231489123/ShareGPT_Vicuna_unfiltered/resolve/main/ShareGPT_V3_unfiltered_cleaned_split.json
